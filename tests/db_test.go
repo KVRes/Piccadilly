839bfa3bd5a025ba14ab3492ac93cb202da8f4f8
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2024-11-13 18:34:03 +0000

    WAL

--- tests/db_test.go
+++ tests/db_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/KVRes/Piccadilly/KV"
 	"github.com/KVRes/Piccadilly/WAL"
 	"github.com/KVRes/Piccadilly/store"
+	"os"
 	"strconv"
 	"testing"
 )
@@ -28,23 +29,17 @@ func initDB() *KV.Bucket {
 	return db
 }
 
-func TestDB(t *testing.T) {
-	db := initDB()
-
+func dataset() map[string]string {
 	m := make(map[string]string)
 	for i := 0; i < 1000; i++ {
 		m["key"+strconv.Itoa(i)] = "value" + strconv.Itoa(i+1)
 	}
-	for k, v := range m {
-		fmt.Println("Setting:", k, "->", v)
-		err := db.Set(k, v)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
+	return m
+}
 
+func inDb(t *testing.T, bucket *KV.Bucket, m map[string]string) {
 	for k, v := range m {
-		val, err := db.Get(k)
+		val, err := bucket.Get(k)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -52,5 +47,66 @@ func TestDB(t *testing.T) {
 			t.Fatal("expected", v, "got", val)
 		}
 	}
+}
+
+func TestDB(t *testing.T) {
+	db := initDB()
+
+	m := dataset()
+	for k, v := range m {
+		err := db.Set(k, v)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	inDb(t, db, m)
+
+	_ = db.Flush()
+}
+
+func TestDBPersist(t *testing.T) {
+	db := initDB()
+
+	m := dataset()
+	inDb(t, db, m)
+}
+
+const WAL_NEED_REC = `
+{"StateOper":"set","Key":"key211","Value":"value212"}
+{"StateOper":"set","Key":"key231","Value":"value232"}
+{"StateOper":"set","Key":"key634","Value":"value635"}
+{"StateOper":"set","Key":"key657","Value":"value658"}`
+
+func TestRecoverFromLog(t *testing.T) {
+	const WAL_F = "WAL_REC.json"
+	const PERSIST_F = "persist_REC.json"
+	// write to log
+	os.WriteFile(WAL_F, []byte(WAL_NEED_REC), 0644)
+	wal, err := WAL.NewJsonWALProvider(WAL_F)
+	if err != nil {
+		panic(err)
+	}
+
+	db := KV.NewBucket(store.NewSwissTableStore(), wal)
+	err = db.StartService(KV.BucketConfig{
+		WALPath:     WAL_F,
+		PersistPath: PERSIST_F,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	m := map[string]string{
+		"key211": "value212",
+		"key231": "value232",
+		"key634": "value635",
+		"key657": "value658",
+	}
+
+	inDb(t, db, m)
+	v, ok := db.Get("key657")
+	fmt.Println(v, ok)
 
 }
